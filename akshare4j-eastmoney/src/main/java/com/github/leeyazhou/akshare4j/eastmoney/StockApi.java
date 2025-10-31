2f11cff77640a7070f7b530aa18a9399e7127828
leeyazhou <bytesgo@163.com>
2025-10-31 17:15:57 +0800

    feat: add tencent

--- akshare4j-eastmoney/src/main/java/com/github/leeyazhou/akshare4j/eastmoney/StockApi.java
+++ akshare4j-eastmoney/src/main/java/com/github/leeyazhou/akshare4j/eastmoney/StockApi.java
@@ -4,12 +4,14 @@
 package com.github.leeyazhou.akshare4j.eastmoney;
 
 import java.math.BigDecimal;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
+import org.apache.commons.lang3.ThreadUtils;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -140,7 +142,8 @@ public class StockApi {
     params.put("invt", "2");
     params.put("fields",
         "f120,f121,f122,f174,f175,f59,f163,f43,f57,f58,f169,f170,f46,f44,f51,f168,f47,f164,f116,f60,f45,f52,f50,f48,f167,f117,f71,f161,f49,f530,f135,f136,f137,f138,f139,f141,f142,f144,f145,f147,f148,f140,f143,f146,f149,f55,f62,f162,f92,f173,f104,f105,f84,f85,f183,f184,f185,f186,f187,f188,f189,f190,f191,f192,f107,f111,f86,f177,f78,f110,f262,f263,f264,f267,f268,f255,f256,f257,f258,f127,f199,f128,f198,f259,f260,f261,f171,f277,f278,f279,f288,f152,f250,f251,f252,f253,f254,f269,f270,f271,f272,f273,f274,f275,f276,f265,f266,f289,f290,f286,f285,f292,f293,f294,f295");
-    // params.put("secid", String.format("%s.%s", querySymbolMap().get(symbol), symbol));
+    // params.put("secid", String.format("%s.%s", querySymbolMap().get(symbol),
+    // symbol));
     params.put("secid", String.format("%s.%s", marketType.getCode(), symbol));
     params.put("_", currentTime());
     context.setParams(params);
@@ -193,6 +196,11 @@ public class StockApi {
       List<StockInfo> stockInfos = new ArrayList<StockInfo>();
       for (;;) {
         params.put("pn", currentPage++);
+        if (currentPage > 1) {
+          int sleep = 3000;
+          ThreadUtils.sleep(Duration.ofSeconds(sleep));
+          logger.info("thread sleep {} ms for next page: {}", sleep, currentPage);
+        }
         HttpResponse httpResponse = HttpUtil.getInstance().get(context, httpClient);
         logger.info("stock_zh_a_spot_em, currentPage: {}, response: {}", currentPage, httpResponse.getResponse());
         EastMoneyResult<DataWrapper<Map<String, String>>> eastMoneyResult = JSON.parseObject(httpResponse.getResponse(),
